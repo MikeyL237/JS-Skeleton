150b9bcaebdbe58c3b00ad0a7fb545fca9051054
MikeyL237 <lawlor101@hotmail.com>
2022-04-21 09:18:06 +0200

    start

---
+++ my-app/client/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import TweetContainer from './Components/tweetcontainer';
+import './App.css';
+
+function App() {
+  return (
+    <TweetContainer name="michael"></TweetContainer>
+  );
+}
+
+export default App;
---
+++ my-app/client/src/Components/tweetcontainer.js
@@ -0,0 +1,9 @@
+const TweetContainer = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+    </div>
+  );
+};
+
+export default TweetContainer
